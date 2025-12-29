f966cb2ff74b2b7be4942bd37049b17080a37618
angmzz <angomez4205@gmail.com>
2025-12-29 02:45:13 -0300

    Algebra Lineal Revision

--- src/capitulo2_algebra_lineal/algebra_lineal.cpp
+++ src/capitulo2_algebra_lineal/algebra_lineal.cpp
@@ -3,31 +3,53 @@
 namespace MathMl {
 namespace LinearAlgebra {
 
+// ## Proyecciones ##
+
+// Proyeccion Ortogonal
+// ## Operaciones ##
+
+// Producto Escalar
 Eigen::VectorXd ProjectVector(const Eigen::VectorXd &x,
                               const Eigen::VectorXd &y) {
   double scalar = x.dot(y) / y.squaredNorm();
   return scalar * y;
 }
 
+// Producto Matricial
 Eigen::MatrixXd MatMul(const Eigen::MatrixXd &A, const Eigen::MatrixXd &B) {
   return A * B;
 }
 
+// ## Resolucion de Sistemas ##
+
+// Resolucion de Sistemas
 Eigen::VectorXd SolveLinearSystem(const Eigen::MatrixXd &A,
                                   const Eigen::VectorXd &b) {
   return A.colPivHouseholderQr().solve(b);
 }
 
+// ## Inversa ##
+
+// Inversa
 Eigen::MatrixXd CalculateInverse(const Eigen::MatrixXd &A) {
   return A.inverse();
 }
 
+// ## Determinante ##
+
+// Determinante
 double CalculateDeterminant(const Eigen::MatrixXd &A) {
   return A.determinant();
 }
 
+// ## Traza ##
+
+// Traza
 double CalculateTrace(const Eigen::MatrixXd &A) { return A.trace(); }
 
+// ## Rango ##
+
+// Rango
 int CalculateRank(const Eigen::MatrixXd &A) {
   return A.colPivHouseholderQr().rank();
 }
